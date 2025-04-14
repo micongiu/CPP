98ab198b2eb7ea6f211673e788b433a4e381e6f5
Orobianco <giucon.mike@outlook.it>
2025-04-14 14:49:04 +0200

    module04 ex00 done

--- cpp_module_4/ex00/main.cpp
+++ cpp_module_4/ex00/main.cpp
@@ -1,21 +1,75 @@
 #include "include/Animal.hpp"
 #include "include/Cat.hpp"
 #include "include/Dog.hpp"
+#include "include/WrongAnimal.hpp"
+#include "include/WrongCat.hpp"
+
+
 
 int main()
 {
+	std::cout << " --- CORRECT ANIMAL --- " << std::endl;
+	std::cout << "Animal object creation: " << std::endl;
 	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
+	std::cout << std::endl;
 
+	std::cout << "Dog object creation: " << std::endl;
+	const Animal* j = new Dog();
+	std::cout << std::endl;
+
+	std::cout << "Cat object creation: " << std::endl;
+	const Animal* i = new Cat();
+	std::cout << std::endl;
+
+	std::cout << "getType calls from Dog and Cat: " << std::endl;
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
+	std::cout << std::endl;
+	
+	std::cout << "makeSound calls from Dog, Cat and Animal: " << std::endl;
 	j->makeSound();
+	i->makeSound(); //will output the cat sound!
 	meta->makeSound();
+	std::cout << std::endl;
 
+	std::cout << "Deleting objects: " << std::endl;
+	std::cout << "Animal object destruction: " << std::endl;
 	delete meta;
+	std::cout << std::endl;
+
+	std::cout << "Dog object destruction: " << std::endl;
 	delete j;
+	std::cout << std::endl;
+
+	std::cout << "Cat object destruction: " << std::endl;
 	delete i;
+
+
+
+	std::cout << std::endl << std::endl;
+	std::cout << " ------------------------------ " << std::endl;
+	std::cout << " --- WRONG ANIMAL --- " << std::endl;
+	const WrongAnimal* meta2 = new WrongAnimal();
+	std::cout << std::endl;
+
+	std::cout << "WrongCat object creation: " << std::endl;
+	const WrongAnimal* j2 = new WrongCat();
+	std::cout << std::endl;
+	
+	std::cout << "getType calls from WrongCat: " << std::endl;
+	std::cout << j2->getType() << " ";
+	std::cout << "makeSound calls from WrongCat: ";
+	j2->makeSound(); //will output the wrongAnimal sound!
+	std::cout << "makeSound call from the WrongCat object: ";
+	meta2->makeSound();
+	std::cout << std::endl;
+
+	std::cout << "WrongAnimal object destruction: " << std::endl;
+	delete meta2;
+	std::cout << std::endl;
+
+	std::cout << "WrongCat object destruction:" << std::endl;
+	delete j2;
+
 	return 0;
 }
--- cpp_module_4/ex00/src/Animal.cpp
+++ cpp_module_4/ex00/src/Animal.cpp
@@ -2,13 +2,13 @@
 
 Animal::Animal() : _type("Defaul")
 {
-	std::cout << "-Animal- created" << std::endl;
+	std::cout << "Animal created" << std::endl;
 }
 
 Animal::Animal(const Animal& src) : _type("Defaul")
 {
 	*this = src;
-	std::cout << "-Animal- created as a copy of Animal" << std::endl;
+	std::cout << "Animal created as a copy of Animal" << std::endl;
 }
 
 Animal& Animal::operator=( const Animal& src )
@@ -21,7 +21,7 @@ Animal& Animal::operator=( const Animal& src )
 
 Animal::~Animal()
 {
-	std::cout << "-Animal- destroyed" << std::endl;
+	std::cout << "Animal destroyed" << std::endl;
 };
 
 void Animal::makeSound() const
